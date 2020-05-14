5a2a5926ae339f74771c2507f082e801f2706782
Nolan Kovacik <noltron000@gmail.com>
2020-05-14 15:24:55 -0400

    [upd] using ES2020 not commonjs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
 import Maze from './generator'
 
 export default Maze
-
-
-const newMaze = Maze([1,3,5])
-const generate = newMaze.generator()
-console.log(newMaze.graph.data[0])
-generate.next()
-generate.next()
-generate.next()
-console.log(newMaze.graph.data[0])
